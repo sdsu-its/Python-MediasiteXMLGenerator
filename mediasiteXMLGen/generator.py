74c163867f258ecfd6e69908e24db3d10f350189
Mirza Ishraq Yeahia <mirzaishraq21@gmail.com>
2021-11-12 14:59:28 -0800

    Full XML Template Added

--- mediasiteXMLGen/generator.py
+++ mediasiteXMLGen/generator.py
@@ -3,23 +3,7 @@ import xml.dom.minidom
 import sys
 
 
-
-
-# Days of the week
-
-Sunday    = 'false'
-Monday    = 'false'
-Tuesday   = 'false'
-Wednesday = 'false'
-Thursday  = 'false'
-Friday    = 'false'
-Saturday  = 'false'
-
-
-
-
 def generate(fileName):
-
     classesDf = pd.read_csv(fileName, sep='\t', header=1)
 
     classesDf.rename(columns={'Start Time': 'startTime', 'End Time': 'endTime', 'Schedule #': 'Schedule'}, inplace=True)
@@ -27,12 +11,15 @@ def generate(fileName):
     classesDf.drop(arrValues, inplace=True)
     classesDf.dropna(subset=['Days'], inplace=True)
 
-    daysList      = classesDf['Days'].to_list()
-    startTimeList = classesDf['startTime'].to_list()
-    endTimeList   = classesDf['endTime'].to_list()
+    # Days of the week
 
-    row1 = classesDf.loc[0]
-    row2 = classesDf.loc[1]
+    Sunday = 'false'
+    Monday = 'false'
+    Tuesday = 'false'
+    Wednesday = 'false'
+    Thursday = 'false'
+    Friday = 'false'
+    Saturday = 'false'
 
     def convertStartTime(x):
 
@@ -55,111 +42,102 @@ def generate(fileName):
         totalTime = minutes + int(x[1:])
         return totalTime
 
-
-
     recurenceArray = []
     scheduleArray = []
     subjectArray = []
 
     for row in classesDf.itertuples():
-        schedule    = row.Schedule
-        subject     = row.Subject
-        days        = row.Days
-        startTime   = row.startTime
-        endTime     = row.endTime
+        schedule = row.Schedule
+        subject = row.Subject
+        days = row.Days
+        startTime = row.startTime
+        endTime = row.endTime
 
-        duration      = str(int(endTime - startTime))
+        duration = str(int(endTime - startTime))
         timeInMinutes = convertTime(duration)
         beginDateTime = convertStartTime(startTime)
 
         if days == 'MWF':
-            Sunday     = 'false'
-            Monday     = 'true'
-            Tuesday    = 'false'
-            Wednesday  = 'true'
-            Thursday   = 'false'
-            Friday     = 'true'
-            Saturday   = 'false'
+            Sunday = 'false'
+            Monday = 'true'
+            Tuesday = 'false'
+            Wednesday = 'true'
+            Thursday = 'false'
+            Friday = 'true'
+            Saturday = 'false'
 
         if days == 'TTH':
-            Sunday    = 'false'
-            Monday    = 'false'
-            Tuesday   = 'true'
+            Sunday = 'false'
+            Monday = 'false'
+            Tuesday = 'true'
             Wednesday = 'false'
-            Thursday  = 'true'
-            Friday    = 'false'
-            Saturday  = 'false'
+            Thursday = 'true'
+            Friday = 'false'
+            Saturday = 'false'
 
         if days == 'TTHF':
-            Sunday    = 'false'
-            Monday    = 'false'
-            Tuesday   = 'true'
+            Sunday = 'false'
+            Monday = 'false'
+            Tuesday = 'true'
             Wednesday = 'false'
-            Thursday  = 'true'
-            Friday    = 'true'
-            Saturday  = 'false'
+            Thursday = 'true'
+            Friday = 'true'
+            Saturday = 'false'
 
         if days == 'M':
-            Sunday    = 'false'
-            Monday    = 'true'
-            Tuesday   = 'false'
+            Sunday = 'false'
+            Monday = 'true'
+            Tuesday = 'false'
             Wednesday = 'false'
-            Thursday  = 'false'
-            Friday    = 'false'
-            Saturday  = 'false'
+            Thursday = 'false'
+            Friday = 'false'
+            Saturday = 'false'
 
         if days == 'T':
-            Sunday    = 'false'
-            Monday    = 'false'
-            Tuesday   = 'true'
+            Sunday = 'false'
+            Monday = 'false'
+            Tuesday = 'true'
             Wednesday = 'false'
-            Thursday  = 'false'
-            Friday    = 'false'
-            Saturday  = 'false'
+            Thursday = 'false'
+            Friday = 'false'
+            Saturday = 'false'
 
         if days == 'W':
-            Sunday    = 'false'
-            Monday    = 'false'
-            Tuesday   = 'false'
+            Sunday = 'false'
+            Monday = 'false'
+            Tuesday = 'false'
             Wednesday = 'true'
-            Thursday  = 'false'
-            Friday    = 'false'
-            Saturday  = 'false'
+            Thursday = 'false'
+            Friday = 'false'
+            Saturday = 'false'
 
         if days == 'Th':
-            Sunday    = 'false'
-            Monday    = 'false'
-            Tuesday   = 'false'
+            Sunday = 'false'
+            Monday = 'false'
+            Tuesday = 'false'
             Wednesday = 'false'
-            Thursday  = 'true'
-            Friday    = 'false'
-            Saturday  = 'false'
+            Thursday = 'true'
+            Friday = 'false'
+            Saturday = 'false'
 
         if days == 'F':
-            Sunday    = 'false'
-            Monday    = 'false'
-            Tuesday   = 'false'
+            Sunday = 'false'
+            Monday = 'false'
+            Tuesday = 'false'
             Wednesday = 'false'
-            Thursday  = 'false'
-            Friday    = 'true'
-            Saturday  = 'false'
+            Thursday = 'false'
+            Friday = 'true'
+            Saturday = 'false'
 
-        recurrences = '<RecorderSchedule xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><Recurrences><Recurrence><BeginDateTime>2022-01-01T{}:00:00</BeginDateTime><EndDateTime>2022-05-30T18:00:00</EndDateTime><RecordingDurationInMinutes>{}</RecordingDurationInMinutes><AlwaysExcludeHolidays>true</AlwaysExcludeHolidays><WeeklySchedule><RecurrenceFrequency>1</RecurrenceFrequency><Sunday>{}</Sunday><Monday>{}</Monday><Tuesday>{}</Tuesday><Wednesday>{}</Wednesday><Thursday>{}</Thursday><Friday>{}</Friday><Saturday>{}</Saturday></WeeklySchedule></Recurrence></Recurrences></RecorderSchedule>'.format(
-            beginDateTime,
-            timeInMinutes, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
+        recurrences = '<?xml version="1.0" encoding="utf-8" ?><RecorderScheduleImport xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><RecorderSchedules><RecorderSchedule><RecorderName>My Recorder</RecorderName><PresentationNamingFormat>ScheduleNameAndNumber</PresentationNamingFormat><AdvanceCreationTimeInMinutes>60</AdvanceCreationTimeInMinutes><AdvanceLoadTimeInMinutes>5</AdvanceLoadTimeInMinutes><ScheduledOperations>None</ScheduledOperations><NotifyPresenters>true</NotifyPresenters><NotificationEmailAddresses><NotificationEmailAddress>notification_1@example.com</NotificationEmailAddress><NotificationEmailAddress>notification_2@example.com</NotificationEmailAddress></NotificationEmailAddresses><DeleteInactive>true</DeleteInactive><Modules><ModuleOverride><ModuleId>test::module::1</ModuleId><ModuleName>Test Module From Import</ModuleName></ModuleOverride><ModuleOverride><ModuleId>test::module::2</ModuleId><Permissions><AceEntry><DirectoryEntry>schedule::import::role</DirectoryEntry><Permission>3</Permission></AceEntry></Permissions></ModuleOverride></Modules><Recurrences><Recurrence><BeginDateTime>2022-01-01T{}:00:00</BeginDateTime><EndDateTime>2022-05-30T18:00:00</EndDateTime><RecordingDurationInMinutes>{}</RecordingDurationInMinutes><AlwaysExcludeHolidays>true</AlwaysExcludeHolidays><WeeklySchedule><RecurrenceFrequency>1</RecurrenceFrequency><Sunday>{}</Sunday><Monday>{}</Monday><Tuesday>{}</Tuesday><Wednesday>{}</Wednesday><Thursday>{}</Thursday><Friday>{}</Friday><Saturday>{}</Saturday></WeeklySchedule></Recurrence></Recurrences></RecorderSchedule></RecorderSchedules></RecorderScheduleImport>'.format(beginDateTime, timeInMinutes, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
         myXML = xml.dom.minidom.parseString(recurrences)
         recurrencData = myXML.toprettyxml()
 
+        print(recurrencData)
+
+        original_stdout = sys.stdout
+
         with open('{}_{}.xml'.format(subject, schedule), 'w') as f:
+            sys.stdout = f
             print(recurrencData)
-
-
-
-
-
-
-
-
-
-
-
+            sys.stdout = original_stdout
